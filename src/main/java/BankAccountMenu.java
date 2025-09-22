168a5a2635451219b8f1415279fc1a5cf4d67277
BraydenV42 <brayriley44@gmail.com>
2025-09-22 13:52:16 -0400

    Update BankAccountMenu.java

--- src/main/java/BankAccountMenu.java
+++ src/main/java/BankAccountMenu.java
@@ -3,27 +3,34 @@ import java.util.Scanner;
 public class FizzBuzzMenu {
     public static void main(String[] args) {
         boolean running = true;
-        
+        double bankAccount = 0.0;
         Scanner sc = new Scanner(System.in);
         
         while(running) {
             int choice;
             System.out.println("------------Bank Account-------------");
-            System.out.println("Add money");
-            System.out.println("Withdraw money");
-            System.out.println("Check balance");
-            System.out.println("Exit");
+            System.out.println("1. Add money");
+            System.out.println("2. Withdraw money");
+            System.out.println("3. Check balance");
+            System.out.println("4. Exit");
             System.out.println("--------------------------------------");
             System.out.println("Whats your choice?");
             choice = sc.nextInt();
+            switch(choice) {
                 case 1:
-                    bankAccount += sc.nextDouble;
+                    System.out.println("What amount do you want to add?");
+                    double adding = sc.nextDouble();
+                    bankAccount = bankAccount + adding;
+                    System.out.println("Result: " + bankAccount);
                     break;
                 case 2:
-                    bankAccount -= sc.nextDouble;
+                    System.out.println("What amount are you withdrawing?");
+                    double minus = sc.nextDouble();
+                    bankAccount = bankAccount - minus;
+                    System.out.println("Result: " + bankAccount);
                     break;
                 case 3: 
-                    System.out.println(bankAccount);
+                    System.out.println("You have " + bankAccount + " dollars.");
                     break;
                 case 4:
                     running = false;
