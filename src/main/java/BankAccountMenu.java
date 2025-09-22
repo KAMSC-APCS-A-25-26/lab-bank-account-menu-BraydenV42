b88ff52a33a4e3fd33f94b972c83721adddc4239
BraydenV42 <brayriley44@gmail.com>
2025-09-22 13:36:09 -0400

    Update BankAccountMenu.java

--- src/main/java/BankAccountMenu.java
+++ src/main/java/BankAccountMenu.java
@@ -1,13 +1,35 @@
 import java.util.Scanner;
 
-public class BankAccountMenu {
+public class FizzBuzzMenu {
     public static void main(String[] args) {
-        // TODO: Implement the bank account menu
-        // 1. Create a double variable for balance
-        // 2. Create a while loop for the menu
-        // 3. Display the menu options
-        // 4. Use Scanner to read user input
-        // 5. Use switch statement to handle menu choices
-        // 6. Implement add money, withdraw money, check balance, and exit functionality
+        boolean running = true;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while(running) {
+            int choice;
+            System.out.println("------------Bank Account-------------");
+            System.out.println("Add money");
+            System.out.println("Withdraw money");
+            System.out.println("Check balance");
+            System.out.println("Exit");
+            System.out.println("--------------------------------------");
+            System.out.println("Whats your choice?");
+            choice = sc.nextInt();
+                case 1:
+                    bankAccount += sc.nextDouble;
+                    break;
+                case 2:
+                    bankAccount -= sc.nextDouble;
+                    break;
+                case 3: 
+                    System.out.println(bankAccount);
+                    break;
+                case 4:
+                    running = false;
+                    System.out.println("Goodbye");
+                    break;
+            }
+        }
     }
 }
